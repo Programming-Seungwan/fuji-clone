8811e917b983d8d1468c55ec2db8ec1b8bfb8923
Programming-Seungwan <swsj4480@gmail.com>
2024-10-07 00:21:26 +0900

    Feat : Google Noto Sans font apply

--- src/style/GlobalStyle.ts
+++ src/style/GlobalStyle.ts
@@ -9,6 +9,11 @@ ${reset}
 }
 
 html, body {
+ /* Noto Sans 웹 폰트 가져오기 */
+ @import url('https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&display=swap');
+
+/* 글로벌 스타일 적용 */
+  font-family: 'Noto Sans', sans-serif;
   min-width: 380px;
   max-width: 1375px;
   margin: 0 auto;
