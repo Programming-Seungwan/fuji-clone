bc4911b3e59dd29fec9fc7def71337c423a94b4b
Programming-Seungwan <swsj4480@gmail.com>
2024-10-08 00:57:30 +0900

    Assets : add Image assets for landing page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,26 @@
 import styled from 'styled-components';
+import HeroCarousel from '@components/home/HeroCarousel';
+import NewProducts from '@components/home/NewProduct';
+import Promotion from '@components/home/Promotion';
+import Lens from '@components/home/Lens';
+import Camera from '@components/home/Camera';
+import Accessory from '@components/home/Accessory';
+import Projector from '@components/home/Projector';
+import OriginalGoods from '@components/home/OriginalGoods';
 
 export default function Home() {
-  return <HomeContainer>Main content</HomeContainer>;
+  return (
+    <HomeContainer>
+      <HeroCarousel />
+      <NewProducts />
+      <Promotion />
+      <Lens />
+      <Camera />
+      <Accessory />
+      <Projector />
+      <OriginalGoods />
+    </HomeContainer>
+  );
 }
 
 const HomeContainer = styled.section`
